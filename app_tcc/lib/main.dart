132b1136d78d4526d7804e8537c59b9cca615ef2
rafaelfsnT <rafaelf_santos08@outlook.com>
2025-08-20 22:07:12 -0300

    estruturando arquivos

--- app_tcc/lib/main.dart
+++ app_tcc/lib/main.dart
@@ -1,20 +1,12 @@
+import 'package:app_tcc/app/my_app.dart';
 import 'package:app_tcc/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-void main() async  {
-   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(
-    const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello, World!', style: TextStyle(fontSize: 24)),
-        ),
-      ),
-    ),
-  );
+void main() async {
+  setUrlStrategy(PathUrlStrategy()); // Remove a # que fica na URL
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
 }
